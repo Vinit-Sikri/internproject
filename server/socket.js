69b67be20d0438f775cf61330448928dd97bf4dd
Vinit Sikri <vinitsikri155@gmail.com>
2024-07-13 19:36:15 +0530

    add files

---
+++ server/socket.js
@@ -0,0 +1,32 @@
+import http from 'http';
+import { Server } from 'socket.io';
+
+const createSocketServer = (app) => {
+  const server = http.createServer(app);
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    socket.emit("me", socket.id);
+
+    socket.on("disconnect", () => {
+      socket.broadcast.emit("callEnded");
+    });
+
+    socket.on("callUser", (data) => {
+      io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name });
+    });
+
+    socket.on("answerCall", (data) => {
+      io.to(data.to).emit("callAccepted", data.signal);
+    });
+  });
+
+  return server;
+};
+
+export default createSocketServer;
